7efb43589e48afa48892b3cbbdb9664811fb8e6c
leonardCarpentier <leonard.carpentier@gmail.com>
2016-01-30 15:12:54 +0100

    MOVED MOAR STUFF

---
+++ GGJ16/Assets/Scenes/Sandboxes/LeonardDebugCtrl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeonardDebugCtrl : MonoBehaviour {
+
+    public Plant yoloPlante;
+
+	// Use this for initialization
+	void Start () {
+        yoloPlante.Grow();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            //Here
+        }
+	}
+}
