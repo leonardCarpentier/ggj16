c30b8d6ec19388fe5cd4e2dd23eb19fb365e96ff
leonardCarpentier <leonard.carpentier@gmail.com>
2016-01-30 13:09:51 +0100

    TestScene + Shaft model

---
+++ GGJ16/Assets/Scripts/Shaft.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Shaft : MonoBehaviour {
+
+	List<Vector2> attachmentSlots = new List<Vector2>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
