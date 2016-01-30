17616fb4bbe16a384d4bcb9b79a8d4fc9c8ef8e2
Yakka <emmanuel.corno@neuf.fr>
2016-01-30 13:54:01 +0100

    Rendering a shaft!

--- GGJ16/Assets/Scripts/ShaftPlane.cs
+++ GGJ16/Assets/Scripts/ShaftPlane.cs
@@ -1,23 +1,50 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
-using UnityEditor;
-
 public class ShaftPlane : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float width = HandleUtility.GetHandleSize(Vector3.zero) * 0.1f;
-        Handles.DrawBezier(transform.position,
-                    Vector3.zero,
-                    Vector3.up,
-                    -Vector3.up,
-                    Color.red,
-                    null,
-                    width);
+    public AnimationCurve shaftCurve;
+
+    // Use this for initialization
+    void Start() {
+        // Create Vector2 vertices
+        Vector2[] vertices2D = new Vector2[] {
+            new Vector2(20,0),
+            new Vector2(13,50),
+            new Vector2(0,100),
+            new Vector2(-13,50),
+            new Vector2(0,0),
+        };
+        Vector2[] vertices2D1 = new Vector2[] {
+            new Vector2(shaftCurve.Evaluate(0f)*20f,0f),
+            new Vector2(shaftCurve.Evaluate(0.5f)*20f,50),
+            new Vector2(shaftCurve.Evaluate(1f)*20f,100),
+            new Vector2(shaftCurve.Evaluate(0.5f)*20f,50)
+        };
+
+        for(int i = 0; i < vertices2D.Length; i++) {
+            Debug.Log(vertices2D[i]);
+        }
+
+        // Use the triangulator to get indices for creating triangles
+        Triangulator tr = new Triangulator(vertices2D);
+        int[] indices = tr.Triangulate();
+
+        // Create the Vector3 vertices
+        Vector3[] vertices = new Vector3[vertices2D.Length];
+        for (int i = 0; i < vertices.Length; i++) {
+            vertices[i] = new Vector3(vertices2D[i].x, vertices2D[i].y, 0);
+        }
+
+        // Create the mesh
+        Mesh msh = new Mesh();
+        msh.vertices = vertices;
+        msh.triangles = indices;
+        msh.RecalculateNormals();
+        msh.RecalculateBounds();
+
+        // Set up game object with mesh;
+        gameObject.AddComponent(typeof(MeshRenderer));
+        MeshFilter filter = gameObject.AddComponent(typeof(MeshFilter)) as MeshFilter;
+        filter.mesh = msh;
     }
 }
