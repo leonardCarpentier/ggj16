7f15643a08c0fe66364b86cc8510bc0a57252f61
leonardCarpentier <leonard.carpentier@gmail.com>
2016-01-30 12:55:23 +0100

    Creating a class for the plant

---
+++ GGJ16/Assets/Scripts/Plant.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Plant : MonoBehaviour {
+
+	//private Attachment rootAttachment;
+
+	string binomialGenus;
+	string binomialSpecies;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Grow(){
+		//Find an empty attachment slot and grow a new shaft on it
+	}
+
+	public void Harvest(){
+		//remove a fruit and return a seed
+	}
+
+	public string Name{
+		get
+    {
+        return this.binomialGenus+" "+this.binomialSpecies;
+    }
+    set
+    {
+        this.binomialSpecies = value;
+    }
+	}
+}
